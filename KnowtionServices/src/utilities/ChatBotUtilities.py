b76f36f89b7159aadbabfa63e357cefe1aed203d
surendra2chandu <surendra2chandu@gmail.com>
2025-05-16 04:59:29 -0700

    Update ChatBotUtilities.py

---
+++ KnowtionServices/src/utilities/ChatBotUtilities.py
@@ -0,0 +1,45 @@
+# Import necessary libraries
+from src.conf.Configurations import THRESHOLD_FOR_SEMANTIC_RETRIVAL, THRESHOLD_FOR_TF_IDF, logger
+
+class ChatBotUtilities:
+    def __init__(self):
+        pass
+
+    def get_sematic_similar_documents_text(self, similar_documents):
+        """
+        Function to get the text from the semantically similar documents
+        :param similar_documents: The similar documents
+        :return: The text from the similar documents
+        """
+
+        # Initialize the context
+        context = ""
+
+        # Get the text from the semantically similar documents
+        logger.info("Getting the text from the semantically similar documents...")
+        for doc in similar_documents["semantic_similar_documents"]:
+            if doc[1] >= THRESHOLD_FOR_SEMANTIC_RETRIVAL:
+                context += doc[0] + " "
+
+        # Return the context
+        return context
+
+
+    def get_tf_idf_similar_documents_text(self, similar_documents):
+        """
+        Function to get the text from the Tf-Idf similar documents
+        :param similar_documents: The similar documents
+        :return: The text from the similar documents
+        """
+
+        # Initialize the context
+        context = ""
+
+        # Get the text from the Tf-Idf similar documents
+        logger.info("Getting the text from the Tf-Idf similar documents...")
+        for doc in similar_documents["tf_idf_similar_documents"]:
+            if doc[1] >= THRESHOLD_FOR_TF_IDF:
+                context += doc[0] + " "
+
+        # Return the context
+        return context
